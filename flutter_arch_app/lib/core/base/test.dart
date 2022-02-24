9669a581e9edb8756c7b8d47dd0b2d424b044c18
SouL-H <mhmtaydinw@gmail.com>
2022-02-24 22:59:33 +0300

    Basic Page start

---
+++ flutter_arch_app/lib/core/base/test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class basePage extends StatefulWidget {
+  basePage({Key? key}) : super(key: key);
+
+  @override
+  State<basePage> createState() => _basePageState();
+}
+
+class _basePageState extends State<basePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
